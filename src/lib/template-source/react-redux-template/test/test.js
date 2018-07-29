a704fb0c68deb4944419e4d9600009f2288c5237
ouvenzhang <1025224452@qq.com>
2018-07-29 14:15:40 +0800

    单元测试用例完善

--- src/lib/template-source/react-redux-template/test/test.js
+++ src/lib/template-source/react-redux-template/test/test.js
@@ -7,7 +7,7 @@ import ${_Component} from '../src/index.jsx';
 import initProps from '../data/index';
 
 import reducer from '../src/reducer';
-import { dispatchAsyncChange } from '../src/action';
+import { dispatchChange, dispatchAsyncChange, dispatchPromiseChange } from '../src/action';
 
 import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
@@ -18,6 +18,11 @@ const store = createStore(reducer, {}, applyMiddleware(...[reduxThunk, reduxProm
 
 Enzyme.configure({ adapter: new Adapter() });
 
+const context = {
+    context: {
+        store: store
+    }
+};
 
 // component 测试
 describe('<${_Component} {...initProps}/> 组件测试', () => {
@@ -28,15 +33,32 @@ describe('<${_Component} {...initProps}/> 组件测试', () => {
     });
 });
 
-// reducer测试
+// Action测试
 describe('<${_Component}> Action测试', () => {
-    it('Test Action success', () => {
 
-        let promise = dispatchAsyncChange.bind({
-            context: {
-                store: store
-            }
-        })('change');
+    it('${_Component} Action dispatch success', () => {
+
+        let promise = dispatchChange.bind(context)('change');
+
+        let changeStatus = store.getState();
+
+        expect(changeStatus).to.have.property('text').equal('change');
+    });
+
+    it('${_Component} Action asyncDispatch success', () => {
+
+        let promise = dispatchAsyncChange.bind(context)('change');
+
+        return promise.then(() => {
+            let change = store.getState();
+
+            expect(change).to.have.property('text');
+        });
+    });
+
+    it('${_Component} Action PromiseDispatch success', () => {
+
+        let promise = dispatchAsyncChange.bind(context)('change');
 
         return promise.then(() => {
             let change = store.getState();
@@ -48,7 +70,7 @@ describe('<${_Component}> Action测试', () => {
 
 // reducer测试
 describe('<${_Component}> reducer测试', () => {
-    it('Test reducer success', () => {
+    it('${_Component} reducer success', () => {
 
         let changeState = reducer({}, {
             type: 'change',
